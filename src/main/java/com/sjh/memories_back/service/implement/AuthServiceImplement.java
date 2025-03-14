17d67ca0805d64ec13adcd0202bb2df7eb9aef16
devJiraynor <jiraynor@naver.com>
2025-03-14 10:49:22 +0900

    2025.03.14. 10:50

---
+++ src/main/java/com/sjh/memories_back/service/implement/AuthServiceImplement.java
@@ -0,0 +1,21 @@
+package com.sjh.memories_back.service.implement;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.sjh.memories_back.common.dto.request.auth.IdCheckRequestDto;
+import com.sjh.memories_back.common.dto.response.ResponseDto;
+import com.sjh.memories_back.service.AuthService;
+
+@Service
+public class AuthServiceImplement implements AuthService {
+
+  @Override
+  public ResponseEntity<ResponseDto> idCheck(IdCheckRequestDto dto) {
+    
+
+    return ResponseDto.success(HttpStatus.OK);
+  }
+  
+}
