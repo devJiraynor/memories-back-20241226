84b1ad49c184d32d7bf5a2bf0223dfccbcdb87c8
devJiraynor <jiraynor@naver.com>
2025-03-18 09:49:31 +0900

    2024.03.18. 09:50

---
+++ src/main/java/com/sjh/memories_back/service/implement/DiaryServiceImplement.java
@@ -0,0 +1,10 @@
+package com.sjh.memories_back.service.implement;
+
+import org.springframework.stereotype.Service;
+
+import com.sjh.memories_back.service.DiarySerivce;
+
+@Service
+public class DiaryServiceImplement implements DiarySerivce {
+  
+}
