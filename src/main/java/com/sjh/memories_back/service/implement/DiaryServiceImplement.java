397f5cf91f64bffc9a30eb0d5b00ba74757b0bcf
devJiraynor <jiraynor@naver.com>
2025-03-18 11:52:40 +0900

    2024.03.18. 11:50

--- src/main/java/com/sjh/memories_back/service/implement/DiaryServiceImplement.java
+++ src/main/java/com/sjh/memories_back/service/implement/DiaryServiceImplement.java
@@ -1,11 +1,15 @@
 package com.sjh.memories_back.service.implement;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.sjh.memories_back.common.dto.request.diary.PostDiaryRequestDto;
 import com.sjh.memories_back.common.dto.response.ResponseDto;
+import com.sjh.memories_back.common.dto.response.diary.GetMyDiaryResponseDto;
 import com.sjh.memories_back.common.entity.DiaryEntity;
 import com.sjh.memories_back.repository.DiaryRepository;
 import com.sjh.memories_back.service.DiarySerivce;
@@ -32,6 +36,24 @@ public class DiaryServiceImplement implements DiarySerivce {
     }
 
     return ResponseDto.success(HttpStatus.CREATED);
+
+  }
+
+  @Override
+  public ResponseEntity<? super GetMyDiaryResponseDto> getMyDiary(String userId) {
+
+    List<DiaryEntity> diaryEntities = new ArrayList<>();
+    
+    try {
+
+      diaryEntities = diaryRepository.findByUserId(userId);
+
+    } catch(Exception exception) {
+      exception.printStackTrace();
+      return ResponseDto.databaseError();
+    }
+
+    return GetMyDiaryResponseDto.success(diaryEntities);
 
   }
   
